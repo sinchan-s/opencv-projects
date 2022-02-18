6e6015b9268f3b21e7736321615b5607d4adb997
Sinchan <sinchan.tex@outlook.com>
2022-02-18 21:07:42 +0530

    eighth

--- cv_chapter5.py
+++ cv_chapter5.py
@@ -1,19 +1,34 @@
 import cv2
 import numpy as np
 
-def empty():
+def empty(z):
+    pass
 
 
 path = "D:\Pictures\lambo.png"
 cv2.namedWindow("TrackBars")
-cv2.resize("TrackBars", 640, 240)
+cv2.resizeWindow("TrackBars", 640, 240)
 cv2.createTrackbar("Hue Min", "TrackBars", 0, 179, empty)
+cv2.createTrackbar("Hue Max", "TrackBars", 179, 179, empty)
+cv2.createTrackbar("Sat Min", "TrackBars", 0, 255, empty)
+cv2.createTrackbar("Sat Max", "TrackBars", 255, 255, empty)
+cv2.createTrackbar("Val Min", "TrackBars", 0, 255, empty)
+cv2.createTrackbar("Val Max", "TrackBars", 255, 255, empty)
 
-img = cv2.imread(path)
+while True:
+    img = cv2.imread(path)
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h_min = cv2.getTrackbarPos("Hue Min", "TrackBars")
+    h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+    s_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
+    s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+    v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+    v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+    lower = np.array([h_min,s_min,v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(imgHSV, lower, upper)
 
-imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-cv2.imshow("image", img)
-cv2.imshow("image-hsv", imgHSV)
-
-cv2.waitKey(0)
+    cv2.imshow("image", img)
+    cv2.imshow("image-hsv", imgHSV)
+    cv2.imshow("image-mask", mask)
+    cv2.waitKey(1)
